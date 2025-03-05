8762993663f57b9bd0514d0694d7056fd6a18e5c
Rody10 <rodneysimbatholanah@gmail.com>
2025-03-05 18:58:22 +0200

    added database creation migration

---
+++ ExcelReader/ExcelReader/EFCoreEmployeeContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelReader
+{
+    internal class EFCoreEmployeeContext: DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb; Database=ExcelReader;Trusted_Connection=True;");
+        }
+
+    }
+}
---
+++ ExcelReader/ExcelReader/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelReader
+{
+    internal class Employee
+    {
+        public int EmployeeID { get; set; }
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Department { get; set; }
+
+        public DateTime HireDate { get; set; }
+
+    }
+}
